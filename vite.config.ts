5ece81f517fbb5f43788fccbba1745f786a84565
Nina Lin <nina@wu2.tw>
2026-04-14 21:37:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/noodle-craft-flow/', // 這是你的儲存庫名稱，前後都要有斜線
-  plugins: [react()],
-  // ...其他原始設定
-}) => ({
+export default defineConfig(({ mode }) => ({
+  base: '/noodle-craft-flow/',
   server: {
     host: "::",
     port: 8080,
